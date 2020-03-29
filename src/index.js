ea2e99742812fe759ef9cb27ed8a3bf4a0cdd8b9
Roopesh <nroopeshkumar@gmail.com>
2020-03-30 01:56:00 +0530

    Basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const store = createStore(productReducer);
 
 ReactDOM.render(
 	<Provider store={store}>
-		<BrowserRouter>
+		<BrowserRouter basename={`${process.env.PUBLIC_URL}/`}>
 			<App />
 		</BrowserRouter>
 	</Provider>,
