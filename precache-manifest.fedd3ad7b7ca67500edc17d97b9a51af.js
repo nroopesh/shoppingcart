25869bfcb25a2442a6589f5da22608ecbe6c4f9b
Roopesh <nroopeshkumar@gmail.com>
2020-03-30 01:33:57 +0530

    Updates

---
+++ precache-manifest.fedd3ad7b7ca67500edc17d97b9a51af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a07a258cb7ae5f0b49ce94b8c82e155c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f2caeed22af1e0b5b9d",
+    "url": "/static/css/2.77b93774.chunk.css"
+  },
+  {
+    "revision": "a1725f4a91b17cef0475",
+    "url": "/static/css/main.6ca6568f.chunk.css"
+  },
+  {
+    "revision": "7f2caeed22af1e0b5b9d",
+    "url": "/static/js/2.8c57f4e6.chunk.js"
+  },
+  {
+    "revision": "5ab1baa52e351a54fb487f180027ee45",
+    "url": "/static/js/2.8c57f4e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1725f4a91b17cef0475",
+    "url": "/static/js/main.2be37876.chunk.js"
+  },
+  {
+    "revision": "5291d243cc70d8ebd72a",
+    "url": "/static/js/runtime-main.a4a46b6e.js"
+  }
+]);
