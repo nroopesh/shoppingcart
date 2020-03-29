6a0e9e233e0552b6b1b3cb5ee1cc3350982a5b91
Roopesh <nroopeshkumar@gmail.com>
2020-03-30 02:05:22 +0530

    Removed base

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const store = createStore(productReducer);
 
 ReactDOM.render(
 	<Provider store={store}>
-		<BrowserRouter basename={`${process.env.PUBLIC_URL}/`}>
+		<BrowserRouter>
 			<App />
 		</BrowserRouter>
 	</Provider>,
